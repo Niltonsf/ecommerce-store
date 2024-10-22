97a64b0d92a21b7fe3087b1ecabe58904332763e
Niltonsf <nilton.schumacher.filho@gmail.com>
2024-10-22 17:50:38 -0300

    feat: expand item

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,21 +2,29 @@
 
 import { Product } from '@/types'
 import Image from 'next/image'
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 import IconButton from './icon-button'
 import { Expand, ShoppingCart } from 'lucide-react'
 import Currency from './currency'
 import { useRouter } from 'next/navigation'
+import usePreviewModal from '@/hooks/use-preview-modal'
 
 interface ProductCardProps {
   data: Product
 }
 
 const ProductCard = ({ data }: ProductCardProps) => {
+  const previewModal = usePreviewModal()
   const router = useRouter()
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`)
+  }
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation()
+
+    previewModal.onOpen(data)
   }
 
   return (
@@ -35,7 +43,7 @@ const ProductCard = ({ data }: ProductCardProps) => {
           <div className="flex gap-x-6 justify-center">
             <IconButton
               icon={<Expand size={20} className="text-gray-600" />}
-              onClick={() => {}}
+              onClick={onPreview}
             />
             <IconButton
               icon={<ShoppingCart size={20} className="text-gray-600" />}
